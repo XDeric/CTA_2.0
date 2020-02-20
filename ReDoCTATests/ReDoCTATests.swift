f8284fc6f8f54b2e7c953fcf6ac8b82dd791f9be
eeeric3995@yahoo.com <ihxx2s7tbu0>
2020-02-20 12:52:29 -0500

    made unit test for rijks museum for to see if data loads

--- ReDoCTATests/ReDoCTATests.swift
+++ ReDoCTATests/ReDoCTATests.swift
@@ -10,25 +10,25 @@ import XCTest
 @testable import ReDoCTA
 
 class ReDoCTATests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    private func getDataFromJSON(name: String) -> Data {
+        guard let pathToData = Bundle.main.path(forResource: name , ofType: "json") else { fatalError("couldnt find json file called \(name).json")}
+        let url = URL(fileURLWithPath: pathToData)
+        do {
+            let data = try Data(contentsOf: url)
+            return data
+        } catch let jsonError {
+            fatalError("couldnt get data from json file \(jsonError)")
         }
     }
+    
+    func testArtLoaded (){
+        let data = getDataFromJSON(name: "rijks")
+        let testArt = RijksDetailAPI.getArtDetail(from: data)
+        XCTAssertTrue(testArt.self != nil, "museum art failed to load")
+    
+    }
+
+    
 
 }
